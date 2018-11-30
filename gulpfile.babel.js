c513ca470edcd8708e9ea0820349d4e3657db9d5
Prateek Rathore <prateek@Prateeks-MacBook-Pro.local>
2018-12-01 03:26:30 +0530

    add: readFile with fs

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,8 +3,16 @@
 // grab our gulp packages
 import gulp from "gulp";
 import gutil from "gulp-util";
+import { readFileSync } from 'fs';
 
 // create a default task and just log a message
-gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
+gulp.task('default', function () {
+  return gutil.log('Gulp is running!');
+});
+
+
+// create a task to read file
+gulp.task('readFile', function () {
+  let fileContent = readFileSync("package.json", "utf8");
+  return console.log(fileContent)
 });
