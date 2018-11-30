a370d51984be04f555b97e339e65871254c648d0
Prateek Rathore <prateek@Prateeks-MacBook-Pro.local>
2018-11-30 20:39:45 +0530

    add: ES6 support with babel-core & babel-preset-es2015

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,8 @@
 /* File: gulpfile.js */
 
 // grab our gulp packages
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+import gulp from "gulp";
+import gutil from "gulp-util";
 
 // create a default task and just log a message
 gulp.task('default', function() {
